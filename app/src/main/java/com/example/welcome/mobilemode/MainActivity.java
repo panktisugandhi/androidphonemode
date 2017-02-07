01698f7939087715f400830ed8960da2d370af86
panktisugandhi <panktisugandhi143@gmail.com>
2017-02-07 13:32:56 +0530

    Mobile phone mode silent / vibrate / Ringing

---
+++ app/src/main/java/com/example/welcome/mobilemode/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.welcome.mobilemode;
+
+import android.content.Context;
+import android.media.AudioManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button b1,b2,b3;
+    AudioManager am;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.button1);
+        b2=(Button)findViewById(R.id.button2);
+        b3=(Button)findViewById(R.id.button3);
+        am=(AudioManager)getSystemService(Context.AUDIO_SERVICE);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                am.setRingerMode(0);
+            }
+        });
+
+        b2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                am.setRingerMode(1);
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                am.setRingerMode(2);
+            }
+        });
+    }
+}
